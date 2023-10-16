63d03fc501b19abcf3fef8ad1a324d0f42e9ae4e
Philipp Franke <philippfranke@gmail.com>
2023-10-16 22:08:43 +0200

    Kalibration

--- rSkripte/kalibration2.R
+++ rSkripte/kalibration2.R
@@ -1,7 +1,7 @@
 library(tidyverse)
 
 data <- read.table("excel/kalibration.csv",sep=',', fill=TRUE, header=TRUE)
-colnames(data) <- c("Modelldatum","simulation_name", "koeff", "tot_egg", "tot_larva", "tot_juv", "tot_adult", "tot_migrJuv", "tot_migrAdult", "anz_migrJuv", "anz_migrAdult", "catched_adult", "catched_juv", "count_pond", "count_egg", "count_larv", "count_juv", "count_adult", "mean_age_adult","min_age_adult", "max_age_adult","sd_age_adult", "median_age_adult")
+colnames(data) <- c("Modelldatum","simulation", "koeff", "tot_egg", "tot_larva", "tot_juv", "tot_adult", "tot_migrJuv", "tot_migrAdult", "anz_migrJuv", "anz_migrAdult", "count_pond", "count_egg", "count_larv", "count_juv", "count_adult", "min_age_adult", "max_age_adult","mean_age_adult", "median_age_adult","sd_age_adult", "catched_adult", "catched_juv", "min_age_catched", "max_age_catched", "mean_age_catched")
 str(data)
 
 koeff_levels <- as.numeric(levels(as.factor(data$koeff)))
