209296f897034f8e5bd366ed7a883d0e124b22a4
Philipp Franke <philippfranke@gmail.com>
2024-01-15 14:11:14 +0100

    Einleitung: grössere Bilder, Diagramme mit Legende

--- rSkripte/kalibration_density.R
+++ rSkripte/kalibration_density.R
@@ -8,20 +8,15 @@ koeff_levels <- as.numeric(levels(as.factor(data$koeff)))
 min <- c(min(data$catched_adult_tot[data$koeff==koeff_levels[1]]),min(data$catched_adult_tot[data$koeff==koeff_levels[2]]),min(data$catched_adult_tot[data$koeff==koeff_levels[3]]))
 max <- c(max(data$catched_adult_tot[data$koeff==koeff_levels[1]]),max(data$catched_adult_tot[data$koeff==koeff_levels[2]]),max(data$catched_adult_tot[data$koeff==koeff_levels[3]]))
 mean <- c(mean(data$catched_adult_tot[data$koeff==koeff_levels[1]]),mean(data$catched_adult_tot[data$koeff==koeff_levels[2]]),mean(data$catched_adult_tot[data$koeff==koeff_levels[3]]))
-data_graph <- data.frame(koeff_levels,min,max,mean)
+data_graph_kali_dens <- data.frame(koeff_levels,min,max,mean)
 
 # bp_adult <- ggplot(data, aes(group= koeff, x=koeff, y=catched_adult_tot)) + geom_boxplot()
 # bp_adult + scale_x_continuous(trans='log2')
 # bp_adult + scale_x_continuous(trans='log2') + scale_y_continuous(trans='log2')
 # bp_print <- bp_adult + scale_x_continuous(trans='log2', breaks=koeff_levels, name="Koeffizient") + scale_y_continuous(name="Anzahl gefangene Adulte")
 
-graph_density <- ggplot(data_graph, aes(koeff_levels,mean))
-graph <- graph_density  + geom_linerange(aes(ymin=min, ymax=max)) +
-  scale_x_continuous(breaks=koeff_levels, name="Dichtekoeffizient") +
-  scale_y_continuous(name="Gefangene adulte Kammmolche 2021") +
+graph_density <- ggplot(data_graph_kali_dens, aes(koeff_levels,mean,group=koeff_levels)) +
   geom_point() +
-  geom_hline(yintercept=700, linewidth=1, linetype=2) +
-  geom_hline(yintercept=1200, linewidth=1, linetype=2) +
-  ggtitle("Kalibrierung der dichteabhängingen Sterblichkeit")
-
-
+  geom_errorbar(aes(ymin=min,ymax=max),width=0.0002)+
+  scale_x_continuous(breaks=koeff_levels, name="Dichtekoeffizient") +
+  scale_y_continuous(name="Gefangene adulte Kammmolche 2021")
